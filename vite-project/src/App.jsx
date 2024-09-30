e823cf03464e73a494b67c5d47afbf0deb20934b
Jonatan <togo36918@gmail.com>
2024-09-30 17:02:50 -0300

    Por mi parte estuve creando el 'body' para nuestra pagina por el momento falta agregar algunas cosas

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,24 +1,24 @@
-
-import './App.css'
-import Body from './componentes/body'
-import Footer from './componentes/footer'
-import Header from './componentes/header'
+import "./App.css";
+import Body from "./componentes/body";
+import Footer from "./componentes/footer";
+import Header from "./componentes/header";
 
 function App() {
-
-
   return (
     <>
-      <Header/>
-      
-      <Body
-        titulo={"payaso"}    
-      />
-      
-      <Footer/>
+      <Header />
 
+      <Body
+        link={"./imagenes/remera.jpeg"}
+        descripcion={"Remera royal"}
+        parrafo={
+          "Una remera de alta calidad con tela traslucida, anti sudor y comoda"
+        }
+      />
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/componentes/body.jsx
+++ vite-project/src/componentes/body.jsx
@@ -1,9 +1,51 @@
-export default function Body({titulo, link, descripcion, parrafo}){
-    return(
-        <>
+export default function Body({ titulo, link, descripcion, parrafo }) {
+  return (
+    <>
+      <div className="container">
         <h2>{titulo}</h2>
-        <img src={link} alt={descripcion} />
-        <p>{parrafo}</p>
-        </>
-    )
+        <div className="listaDeLado">
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
