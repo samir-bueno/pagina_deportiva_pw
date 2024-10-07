91f2f4faa9d1cf6fdfae925ffdac535aed6d62f5
Jonatan <togo36918@gmail.com>
2024-10-07 16:56:21 -0300

    Se modifico codigo del 'body' y se insertaron nuevas cosas

--- vite-project/src/componentes/body.jsx
+++ vite-project/src/componentes/body.jsx
@@ -2,47 +2,84 @@ export default function Body({ titulo, link, descripcion, parrafo }) {
   return (
     <>
       <div className="container">
+        <div>
+          <img
+            className="product-image elemento animate__animated animate__fadeInDown "
+            src="../src/assets/messi.jpeg"
+            alt="Messi"
+          />
+        </div>
         <h2>{titulo}</h2>
+        <div className="listaDeLado animate__animated animate__bounceInLeft">
+          <div className="containerImg containerImg:hover">
+            <img className="imagen " src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg ">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+          <div className="containerImg">
+            <img className="imagen" src={link} alt={descripcion} />
+            <p className="texto centrar">{parrafo}</p>
+            <p className="num centrar">$12000</p>
+          </div>
+        </div>
         <div className="listaDeLado">
-          <div className="containerImg">
-            <img className="imagen" src={link} alt={descripcion} />
-            <p className="texto centrar">{parrafo}</p>
-            <p className="num centrar">$12000</p>
+          <div>
+            <img
+              className="imagen_marca"
+              src="https://media2.solodeportes.com.ar/media/catalog/brands/Adidas-Performance-inactive.png"
+              alt="Adidas"
+            />
           </div>
-          <div className="containerImg">
-            <img className="imagen" src={link} alt={descripcion} />
-            <p className="texto centrar">{parrafo}</p>
-            <p className="num centrar">$12000</p>
+          <div>
+            <img
+              className="imagen_marca"
+              src="https://media2.solodeportes.com.ar/media/catalog/brands/fila.png"
+              alt="Fila"
+            />
           </div>
-          <div className="containerImg">
-            <img className="imagen" src={link} alt={descripcion} />
-            <p className="texto centrar">{parrafo}</p>
-            <p className="num centrar">$12000</p>
+          <div>
+            <img
+              className="imagen_marca"
+              src="https://media2.solodeportes.com.ar/media/catalog/brands/logo200x200nike.png"
+              alt="Nike"
+            />
           </div>
-          <div className="containerImg">
-            <img className="imagen" src={link} alt={descripcion} />
-            <p className="texto centrar">{parrafo}</p>
-            <p className="num centrar">$12000</p>
-          </div>
-          <div className="containerImg">
-            <img className="imagen" src={link} alt={descripcion} />
-            <p className="texto centrar">{parrafo}</p>
-            <p className="num centrar">$12000</p>
-          </div>
-          <div className="containerImg">
-            <img className="imagen" src={link} alt={descripcion} />
-            <p className="texto centrar">{parrafo}</p>
-            <p className="num centrar">$12000</p>
-          </div>
-          <div className="containerImg">
-            <img className="imagen" src={link} alt={descripcion} />
-            <p className="texto centrar">{parrafo}</p>
-            <p className="num centrar">$12000</p>
-          </div>
-          <div className="containerImg">
-            <img className="imagen" src={link} alt={descripcion} />
-            <p className="texto centrar">{parrafo}</p>
-            <p className="num centrar">$12000</p>
+          <div>
+            <img
+              className="imagen_marca"
+              src="https://media2.solodeportes.com.ar/media/catalog/brands/puma.png"
+              alt="Puma"
+            />
           </div>
         </div>
       </div>
