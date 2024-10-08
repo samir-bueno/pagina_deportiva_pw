b7fd552f4ba4ee77fa74a1b5860375543f6b4a2a
samir-bueno <samirbueno585@gmail.com>
2024-10-08 16:07:37 -0300

    modifique el footer para cambiarle el fondo por un gris y ademas cambiar algunos logos para adaptarlo

--- vite-project/src/componentes/footer.jsx
+++ vite-project/src/componentes/footer.jsx
@@ -5,26 +5,26 @@ export default function Footer() {
         <footer className="footer">
             <div className="contenedor">
                 <div className="columna">
-                    <h4>MÉTODO DE PAGO</h4>
+                    <h4 className="color_blanco">MÉTODO DE PAGO</h4>
                     <img className='tamaño_imagen_1'           
                         src="https://cdn.tiendanegocio.com/static/payment_method/MERCADOPAGO.png" 
                         alt="Mercado Pago" 
                     />
-                    <h4>MÉTODO DE ENVÍO</h4>
+                    <h4 className="color_blanco">MÉTODO DE ENVÍO</h4>
                     <img className='tamaño_imagen_1'
                         src="https://cdn.tiendanegocio.com/static/shipment_method/acordar.png"
                         alt="Punto de encuentro" 
                     />
                 </div>
                 <div className="columna">
-                    <h4 style={{ textAlign: 'center' }}>CONTACTO</h4>
+                    <h4 style={{ textAlign: 'center' }} className="color_blanco">CONTACTO</h4>
                     <div className="contacto-item">
                         <img 
                             className='icono2' 
-                            src="https://cdn-icons-png.flaticon.com/512/561/561127.png" 
+                            src="https://cdn-icons-png.flaticon.com/512/732/732200.png"
                             alt="Email" 
                         />
-                        <span> uniondepo@gmail.com</span>
+                        <span className="color_blanco"> uniondepo@gmail.com</span>
                     </div>
                     <div className="contacto-item">
                         <img 
@@ -32,35 +32,35 @@ export default function Footer() {
                             src="https://cdn-icons-png.flaticon.com/512/724/724664.png" 
                             alt="Teléfono" 
                         />
-                        <span style={{width:"150px"}}> +123 456 7890</span>
+                        <span style={{width:"150px"}} className="color_blanco"> +123 456 7890</span>
                     </div>
                     <div className="contacto-item">
                         <img 
                             className='icono2'  
-                            src="https://cdn-icons-png.flaticon.com/512/1946/1946488.png"
+                            src="https://cdn-icons-png.flaticon.com/512/9713/9713317.png"
                             alt="Ubicación" 
                         />
-                        <span style={{width:"150px"}}> Av. Caballito</span>
+                        <span style={{width:"150px"}} className="color_blanco"> Av. Caballito</span>
                     </div>
                     <div className="contacto-item">
                         <img 
                             className='icono2' 
-                            src="https://cdn-icons-png.flaticon.com/512/67/67657.png"
+                            src="https://cdn-icons-png.flaticon.com/512/9053/9053332.png"
                             alt="Signo de arrepentimiento" 
                         />
-                        <span style={{width:"150px"}}>Arrepentimiento</span>
+                        <span style={{width:"150px"}} className="color_blanco">Arrepentimiento</span>
                     </div>
                 </div>
                 <div className="columna">
-                    <h4>REDES SOCIALES</h4>
+                    <h4 className="color_blanco">REDES SOCIALES</h4>
                     <div className="contacto-item">
                         <img 
                             className='icono2' 
-                            src="https://cdn-icons-png.flaticon.com/512/87/87390.png"                            alt="Facebook" 
+                            src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png"                        alt="Facebook" 
                             height="40"
                             width="40"
                         />
-                        <a href="https://www.instagram.com/camisetas_f.m/" className="redes">Instagram</a>
+                        <a href="https://www.instagram.com/camisetas_f.m/" className="redes color_blanco">Instagram</a>
                     </div>
                 </div>
             </div>
