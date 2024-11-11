aa7d4dfef3d2f6fa0c24f6fb504467e1814fd164
Deybi2 <deybijuaniquina@gmail.com>
2024-11-11 11:55:19 -0300

    Pude  agregar una cuadrícula que esta en la imagen de cada producto que me permite eliminarlo, como tambien pude crear un ruta a talles

--- vite-project/src/componentes/body.jsx
+++ vite-project/src/componentes/body.jsx
@@ -1,19 +1,31 @@
 import { Link } from "react-router-dom";
 
-function Body({ id, titulo, link, description, parrafo, precio }) {
+function Body({ id, titulo, link, description, parrafo, precio, onDelete }) {
+  const handleDelete = () => {
+    if (window.confirm("¿Estás seguro de que quieres eliminar este producto?")) {
+      onDelete(id); // Llamamos a la función onDelete pasada como prop
+    }
+  };
+
   return (
     <div className="listaDeLado">
       <h2>{titulo}</h2>
       <div className="animate__animated animate__bounceInLeft" id="products">
         <Link to={`/product/${id}`}>
-          {" "}
-          {/* Usa el ID aquí */}
           <div className="containerImg">
             <img className="imagen" src={link} alt={description} />
             <p className="texto centrar">{parrafo}</p>
             <p className="num centrar">${precio}</p>
+            
+
+            </div>
+            </Link>
+            {/* Botón para eliminar producto */}
+            <div className="delete-btn" onClick={handleDelete}>
+              <div className="line"></div>
+            </div>
           </div>
-        </Link>
+        <div>
       </div>
     </div>
   );
